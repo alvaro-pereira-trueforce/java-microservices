51e00bc698e43f115049cff634dc3f3763e89ad5
Santiago Mamani <santiago.mamani@assuresoft.com>
2018-04-18 08:48:25 -0400

    Removed route of list mediaComents

--- APIServices/Instagram/routes.php
+++ APIServices/Instagram/routes.php
@@ -1,5 +1,4 @@
 <?php
+
 Route::get('/media/{idMedia}/comments/{token}', 'InstagramController@getMediaComments');
 Route::get('/listMedia/{token}', 'InstagramController@getAllUserMedia');
-//getAllUserMediaAndComments
-Route::get('/listMedia/comments/{token}', 'InstagramController@getAllUserMediaAndComments');
