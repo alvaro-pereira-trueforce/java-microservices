f8982dd3938ecbff09d27e5de016da73a4487cc0
Pablo Daza <pablo.daza@trueforce.com>
2018-04-19 11:37:50 -0400

    - fix add application manifest command to retrieve the new table rows for manifest.

--- app/Console/AddApplicationManifestCommand.php
+++ app/Console/AddApplicationManifestCommand.php
@@ -52,6 +52,9 @@ class AddApplicationManifestCommand extends Command {
         $admin_ui = $admin_ui ? $app_url . $admin_ui : '';
         $this->info($admin_ui);
 
+        $push_client_id = $this->ask('Do you have push Client ID?');
+        $push_client_id = $push_client_id ? $push_client_id : '';
+
         $pull_url = $this->ask('What is the pull URL?');
         $pull_url = $pull_url ? $app_url . $pull_url : '';
         $this->info($pull_url);
@@ -68,6 +71,14 @@ class AddApplicationManifestCommand extends Command {
         $healthcheck_url = $healthcheck_url ? $app_url . $healthcheck_url : '';
         $this->info($healthcheck_url);
 
+        $dashboard_url = $this->ask('Do you have dashboard URL?');
+        $dashboard_url = $dashboard_url ? $app_url . $dashboard_url : '';
+        $this->info($dashboard_url);
+
+        $about_url = $this->ask('Do you have about URL?');
+        $about_url = $about_url ? $app_url . $about_url : '';
+        $this->info($about_url);
+
         DB::beginTransaction();
         try
         {
@@ -75,14 +86,17 @@ class AddApplicationManifestCommand extends Command {
                 'name' => $this->argument('name'),
                 'id' => $this->argument('id'),
                 'author' => $this->argument('author'),
-                'version' => $this->argument('version')
+                'version' => $this->argument('version'),
+                'push_client_id' => $push_client_id
             ],
                 [
                     'admin_ui' => $admin_ui,
                     'pull_url' => $pull_url,
                     'channelback_url' => $channelback_url,
                     'clickthrough_url' => $clickthrough_url,
-                    'healthcheck_url' => $healthcheck_url
+                    'healthcheck_url' => $healthcheck_url,
+                    'dashboard_url' => $dashboard_url,
+                    'about_url' => $about_url
                 ]);
             DB::commit();
             $this->info(sprintf('A new application manifest was created with ID %s', $manifest));
