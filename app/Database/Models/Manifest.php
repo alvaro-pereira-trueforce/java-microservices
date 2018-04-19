b38ce2441eab8701be8194e94f0b5792f1eb941e
Pablo Daza <pablo.daza@trueforce.com>
2018-04-19 12:11:39 -0400

    filter all the empty parameters on manifest for zendesk

--- app/Database/Models/Manifest.php
+++ app/Database/Models/Manifest.php
@@ -4,9 +4,12 @@ namespace App\Database\Models;
 
 
 use App\Database\Eloquent\ModelUUID;
+use App\Database\Traits\toArrayFiltered;
 
 class Manifest extends ModelUUID {
     protected $table = 'manifests';
+
+    use toArrayFiltered;
 
     /**
      * The attributes that are mass assignable.
