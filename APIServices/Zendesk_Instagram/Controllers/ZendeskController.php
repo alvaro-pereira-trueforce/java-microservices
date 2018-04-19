3fd6a994c8ed40417ad946453c22e231f34db676
Santiago Mamani <santiago.mamani@assuresoft.com>
2018-04-19 11:24:12 -0400

    Add Controllers

---
+++ APIServices/Zendesk_Instagram/Controllers/ZendeskController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace APIServices\Zendesk_Instagram\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class ZendeskController extends Controller
+{
+    public function getManifest(){
+
+    }
+}
---
+++ APIServices/Zendesk_Instagram/routes_public.php
@@ -0,0 +1,9 @@
+<?php
+$router->get('/manifestss','ZendeskController@getManifest');
+//$router->post('/admin_ui','ZendeskController@admin_ui');
+//$router->post('/admin_ui_2','ZendeskController@admin_ui_2');
+//$router->post('/pull','ZendeskController@pull');
+//$router->post('/channelback','ZendeskController@channelback');
+//$router->get('/clickthrough','ZendeskController@clickthrough');
+//$router->get('/healthcheck','ZendeskController@healthcheck');
+//$router->post('/event_callback', 'ZendeskController@event_callback');
