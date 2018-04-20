4248cfd375e418274a4b576bd26dfe56a1476f13
Pablo Daza <pablo.daza@trueforce.com>
2018-04-20 15:01:57 -0400

    - hide fake endpoint

--- APIServices/Zendesk_Telegram/routes_public.php
+++ APIServices/Zendesk_Telegram/routes_public.php
@@ -3,7 +3,7 @@
 $router->prefix('telegram')->group(function () use ($router) {
     $router->get('manifest','ZendeskController@getManifest');
     $router->post('admin_ui','ZendeskController@adminUI');
-    $router->get('admin_ui','ZendeskController@adminUI');
+    //$router->get('admin_ui','ZendeskController@adminUI'); //This is just for testing
     $router->post('admin_ui_2','ZendeskController@admin_ui_2');
     $router->post('pull','ZendeskController@pull');
     $router->post('channelback','ZendeskController@channelback');
