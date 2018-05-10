246181bd84ce9dd657284b3c2fb18b4f7bdb1d23
Pablo Daza <pablo.daza@trueforce.com>
2018-05-10 11:31:13 -0400

    final Multimedia implementation

--- APIServices/Zendesk_Telegram/Models/MessageTypes/Audio.php
+++ APIServices/Zendesk_Telegram/Models/MessageTypes/Audio.php
@@ -5,4 +5,23 @@ namespace APIServices\Zendesk_Telegram\Models\MessageTypes;
 
 class Audio extends FilesMessageType {
 
+    function getTransformedMessage() {
+        $audio = $this->message->getAudio();
+        try
+        {
+            if((int) $audio->getFileSize() > 5000000)
+            {
+                $this->telegramService->sendTelegramMessage(
+                    $this->chat_id,
+                    'Our support size limit was reached, please try to send smaller Audios'
+                );
+                return null;
+            }
+            $documentURL = $this->telegramService->getDocumentURL($audio);
+            return $this->getBasicDocumentResponse('Music File Message', $documentURL);
+        }catch (\Exception $exception)
+        {
+            return null;
+        }
+    }
 }
---
+++ resources/views/telegram/maps_viewer.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<body>
+    <p>{{$message}}</p>
+    <a href="http://www.google.com/maps/place/{{$latitude}},{{$longitude}}">Click here to open
+        google maps</a>
+</body>
+</html>
