5a5740b19eb9900164984c07f5f0e0c6f525afbe
Santiago Mamani <santiago.mamani@assuresoft.com>
2018-05-21 18:31:59 -0400

    Commet to test

--- APIServices/Instagram/routes.php
+++ APIServices/Instagram/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+//Comment
 Route::get('/{token}/{idIstagram}/{limit}/listMedia', 'InstagramController@getMedia');
 Route::get('/{token}/media/{idMedia}/{limit}/comment', 'InstagramController@getComment');
 Route::post('/{token}/media/{idMedia}/comment/{message}', 'InstagramController@postComment');
