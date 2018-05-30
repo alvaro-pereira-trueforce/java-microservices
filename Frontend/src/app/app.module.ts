ce46dadaf962871a7bb818546f95d531cc7e20f6
Pablo Daza <pablo.daza@trueforce.com>
2018-05-30 09:12:50 -0400

    New Frontend angular 6

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,33 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-import {AlertModule, BsDropdownModule, DatepickerModule, ModalModule, TooltipModule} from "ngx-bootstrap";
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AlertModule.forRoot(),
-    DatepickerModule.forRoot(),
-    BsDropdownModule.forRoot(),
-    TooltipModule.forRoot(),
-    ModalModule.forRoot()
-  ],
-  exports: [
-    AlertModule,
-    DatepickerModule,
-    BsDropdownModule,
-    TooltipModule,
-    ModalModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        NgbModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+
+export class AppModule {
+}
