dc56140a57b574a9d0ce43fe8019bb3d7b73e7fc
Pablo Daza <pablo.daza@trueforce.com>
2018-07-10 11:15:28 -0400

    Contact style

--- resources/views/telegram/contact_viewer.blade.php
+++ resources/views/telegram/contact_viewer.blade.php
@@ -1,8 +1,16 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en-US">
 <body>
-    <p>{{$message}}</p>
+<p>{{$message}}</p>
+<div style="border-width: 2px;
+border-style: dashed;
+border-color: gray;
+width: auto;
+padding: 15px;
+margin: auto;
+display: inline-block;">
     <label><b>{{$firstName}} {{$lastName}}</b></label>
     <p>{{$phoneNumber}}</p>
+</div>
 </body>
 </html>
