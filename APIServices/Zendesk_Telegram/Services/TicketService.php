47cc43be879b682cc8e65b3067a6f84a7394ced1
Pablo Daza <pablo.daza@trueforce.com>
2019-02-25 15:31:32 -0400

    disable expiration if tickets by group is enabled

--- APIServices/Zendesk_Telegram/Services/TicketService.php
+++ APIServices/Zendesk_Telegram/Services/TicketService.php
@@ -19,15 +19,16 @@ class TicketService
 
     /**
      * @param $parent_id
+     * @param $disable_expiration
      * @return string
      */
-    public function getValidParentID($parent_id)
+    public function getValidParentID($parent_id, $disable_expiration = false)
     {
         $ticket = $this->repository->findByParentID($parent_id);
 
         if ($ticket) {
             $ticket_date = $ticket->updated_at;
-            if ($ticket_date->diffInMinutes(Carbon::now()) >= (int)env('TIME_EXPIRE_FOR_TICKETS_IN_MINUTES_TELEGRAM')) {
+            if (!$disable_expiration && $ticket_date->diffInMinutes(Carbon::now()) >= (int)env('TIME_EXPIRE_FOR_TICKETS_IN_MINUTES_TELEGRAM')) {
                 $ticket->uuid = (string)Uuid::uuid4()->toString();
                 $ticket->save();
             }
